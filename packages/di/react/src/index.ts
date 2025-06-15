3bccc7fbefadf28c53b9cdaa941db932c472e4d3
llucas.spot <llucas.spot@gmail.com>
2025-06-16 01:15:02 +0200

    fix: di react

--- packages/di/react/src/index.ts
+++ packages/di/react/src/index.ts
@@ -1,3 +1,4 @@
 export {DIProvider} from './di-context/di.provider'
 export {useDI} from './di-context/use-di.hook'
 export {useInstance} from './di-context/use-instance.hook'
+export * from '@mygoodstack/di-core'
