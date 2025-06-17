2988cf588520a4f05c2ab1f7bc61121b2c57a6fa
llucas.spot <llucas.spot@gmail.com>
2025-06-17 02:51:13 +0200

    fix: di react build

--- packages/di-react/vite.config.ts
+++ packages/di-react/vite.config.ts
@@ -1,7 +1,33 @@
+import {resolve} from "node:path";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+    dts({
+      insertTypesEntry: true,
+      entryRoot: 'src',
+      tsconfigPath: './tsconfig.app.json',
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    sourcemap: true,
+  },
 })
