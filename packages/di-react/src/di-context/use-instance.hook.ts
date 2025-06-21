076456ac67f220232a08785ee212dae8cebbdc41
llucas.spot <llucas.spot@gmail.com>
2025-06-21 02:39:16 +0200

    fix: di react useInstance use memo

--- packages/di-react/src/di-context/use-instance.hook.ts
+++ packages/di-react/src/di-context/use-instance.hook.ts
@@ -1,7 +1,10 @@
 import {Token} from "@mygoodstack/di-core";
+import {useMemo} from "react";
 import {useDI} from "./use-di.hook";
 
 export function useInstance<T>(token: Token<T>) {
     const {container} = useDI()
-    return container.resolve(token)
+    return useMemo(() => {
+        return container.resolve(token)
+    }, [container])
 }
