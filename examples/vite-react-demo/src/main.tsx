184b78b3481ac310f8006fe870f4c59e30211763
llucas.spot <llucas.spot@gmail.com>
2025-07-07 15:28:36 +0200

    fix: exemples

--- examples/vite-react-demo/src/main.tsx
+++ examples/vite-react-demo/src/main.tsx
@@ -1,5 +1,5 @@
 import 'reflect-metadata';
-import {containerByEnv as containerByEnvCore} from "@mygoodstack/di-core";
+import {containerByEnv as containerByEnvCore, ContainerI} from "@mygoodstack/di-core";
 import {containerByEnv, DIProvider} from "@mygoodstack/di-react";
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
@@ -9,13 +9,15 @@ import App from './App'
 import './i18n';
 import './services/user.module';
 
+const nodeEnv = import.meta.env.VITE_APP_ENV as keyof typeof containerByEnv
+console.log(`process.env.NODE_ENV : ${nodeEnv}`)
+console.log(`containerByEnv equals : ${containerByEnv === containerByEnvCore}`)
+const container: ContainerI = containerByEnv[nodeEnv]
+
+container.consoleLog()
+
 const AppWithDIProvider = () => {
-    const nodeEnv = process.env.NODE_ENV as keyof typeof containerByEnv
-    console.log(`process.env.NODE_ENV : ${nodeEnv}`)
 
-    console.log(`containerByEnv equals : ${containerByEnv === containerByEnvCore}`)
-
-    const container = containerByEnv[nodeEnv]
     return (
         <DIProvider container={container}>
             <App/>
--- examples/vite-vue-demo/src/AppWithProvider.vue
+++ examples/vite-vue-demo/src/AppWithProvider.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-import {containerByEnv} from "@mygoodstack/di-vue";
+import {containerByEnv as containerByEnvCore, containerByEnv, ContainerI} from "@mygoodstack/di-vue";
 import {DIProvider} from "@mygoodstack/di-vue";
 import App from "./App.vue";
 
-const nodeEnv = process.env.NODE_ENV as keyof typeof containerByEnv
+const nodeEnv = import.meta.env.VITE_APP_ENV as keyof typeof containerByEnv
 console.log(`process.env.NODE_ENV : ${nodeEnv}`)
+console.log(`containerByEnv equals : ${containerByEnv === containerByEnvCore}`)
+const container: ContainerI = containerByEnv[nodeEnv]
 
-const container = containerByEnv[nodeEnv]
+container.consoleLog()
 
 </script>
 
